bdeac108371e6374fb841703e4895138f0205965
Mike Preis <mikepreis@users.noreply.github.com>
2020-02-08 13:07:17 -0800

    lab05

---
+++ labs/lab05/src/comparator/ArtistComparator.java
@@ -0,0 +1,10 @@
+package comparator;
+import java.util.Comparator;
+
+public class ArtistComparator implements Comparator<Song> {
+
+    @Override
+    public int compare(Song s1, Song s2) {
+        return s1.getArtist().compareTo(s2.getArtist());
+    }
+}
